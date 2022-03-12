1d41502c6cb38ac7cdf9239bbd2b241f29d11784
Axing <1009641543@qq.com>
2022-03-12 22:06:48 +0800

    Static layout really finished.

--- src/components/ShowList/index.tsx
+++ src/components/ShowList/index.tsx
@@ -5,38 +5,47 @@ const ShowList = () => {
   return (
     <div className="showlist">
       <div className="unfinish">
+        <div className="listtitle">To Do</div>
         <ul>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
         </ul>
       </div>
       <div className="finished">
+      <div className="listtitle">Completed</div>
         <ul>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
           <li className="todoitem">
             <input type="checkbox" className="checkbox"/>
             <span className="discription">discription</span>
+            <i className="iconfont clean" >&#xe8d1;</i>
           </li>
         </ul>
       </div>
