916d01fb894c4360e602a7fa05d008004e8af25e
Axing <1009641543@qq.com>
2022-03-14 20:26:26 +0800

    Data render completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,94 @@
 import * as React from 'react';
 import "./App.scss";
-import Input from "./components/Input";
+import MyInput from "./components/MyInput";
 import ShowList from "./components/ShowList";
 
 type todoItemType = {
   discription: string,
-  done: true | false 
+  done: true | false,
+  id: string
 }
 type todolistType = todoItemType[];
 
 function App() {
+
   const [todolist, setTodolist] = React.useState<todolistType | null>(null);
+
+  function addTodoItem(e: React.KeyboardEvent<HTMLInputElement>): void {
+    if(e.code === "Enter"){
+      const res = (e.target as HTMLInputElement).value;
+      setTodolist((preState): todolistType => {
+        if( preState !== null) {
+          return [
+            ...preState,
+            {
+              discription: res,
+              done: false,
+              id: Date.now() + ""
+            }
+          ]
+        }
+        return [
+          {
+            discription: res,
+            done: false,
+            id: Date.now() + ""
+          }
+        ]
+      })
+    }
+  }
+
+  function changeTodoItem(id: string): void {
+    console.log("changeTodoItem called, id = " + id);
+    setTodolist((preState) => {
+      let changed: todoItemType= {
+        discription: "none",
+        done: false,
+        id: Date.now() + ""
+      };
+      if(preState !== null) {
+        const res = preState.filter((item) => {
+          if(item.id === id) {
+            changed = item;
+            return false
+          }
+          return true
+        })
+        changed.done = !changed.done;
+        console.log(changed);
+        return [
+          ...res,
+          {
+            discription: changed.discription,
+            done: !changed.done,
+            id: changed.id
+          }
+        ] as todolistType;
+      }
+
+      return null;
+    })
+  }
+
+  function deleteTodoItem(id: string): void {
+    setTodolist((preState) => {
+      if(preState !== null) {
+        const res = preState.filter((item) => {
+          return item.id !== id;
+        })
+  
+        return res;
+      }
+      return null;
+    })
+  }
 
   return (
     <div className="App">
       <h2>AXing's TodoList</h2>
-      <Input></Input>
-      <ShowList></ShowList>
+      <MyInput addTodoItem={ addTodoItem }></MyInput>
+      <ShowList todolist={ todolist ? todolist : null } changeTodoItem={ changeTodoItem } deleteTodoItem={ deleteTodoItem }></ShowList>
     </div>
   );
 }
--- src/components/ShowList/index.tsx
+++ src/components/ShowList/index.tsx
@@ -1,52 +1,78 @@
 import * as React from "react";
 import "./ShowList.scss"
 
-const ShowList = () => {
+type todoItemType = {
+  discription: string,
+  done: true | false,
+  id: string
+}
+type todolistType = todoItemType[];
+
+type ShowListProps = {
+  todolist: todolistType | null,
+  changeTodoItem: (id: string) => void,
+  deleteTodoItem: (id: string) => void
+}
+
+const ShowList = ({ todolist, changeTodoItem, deleteTodoItem }: ShowListProps) => {
   return (
     <div className="showlist">
       <div className="unfinish">
         <div className="listtitle">To Do</div>
         <ul>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
+          { todolist ? 
+            todolist.filter((item) => {return !item.done}).length > 0 ?
+            todolist.filter((item) => {return !item.done}).map((item) => {
+              return (
+                <li className="todoitem" key={item.id}>
+                  <input type="checkbox" className="checkbox" onChange={() => {changeTodoItem(item.id)}} checked={item.done}/>
+                  <span className="discription">{item.discription}</span>
+                  <i className="iconfont clean" onClick={() => {deleteTodoItem(item.id)}}>&#xe8d1;</i>
+                </li>
+              )
+            }) : (
+              <li className="todoitem">
+                {/* <input type="checkbox" className="checkbox" onClick={changeTodoItem}/> */}
+                <span className="discription">have no comtent</span>
+                {/* <i className="iconfont clean" onClick={deleteTodoItem}>&#xe8d1;</i> */}
+              </li>
+            )
+              :
+            <li className="todoitem">
+              {/* <input type="checkbox" className="checkbox" onClick={changeTodoItem}/> */}
+              <span className="discription">have no comtent</span>
+              {/* <i className="iconfont clean" onClick={deleteTodoItem}>&#xe8d1;</i> */}
+            </li>
+          }
         </ul>
       </div>
       <div className="finished">
       <div className="listtitle">Completed</div>
         <ul>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
-          <li className="todoitem">
-            <input type="checkbox" className="checkbox"/>
-            <span className="discription">discription</span>
-            <i className="iconfont clean" >&#xe8d1;</i>
-          </li>
+          { todolist ? 
+            todolist.filter((item) => {return item.done}).length > 0 ?
+            todolist.filter((item) => {return item.done}).map((item) => {
+              return (
+                <li className="todoitem" key={item.id}>
+                  <input type="checkbox" className="checkbox" checked={item.done} onChange={() => {changeTodoItem(item.id)}}/>
+                  <span className="discription">{item.discription}</span>
+                  <i className="iconfont clean" onClick={() => {deleteTodoItem(item.id)}}>&#xe8d1;</i>
+                </li>
+              )
+            }) : (
+              <li className="todoitem">
+                {/* <input type="checkbox" className="checkbox" onClick={changeTodoItem}/> */}
+                <span className="discription">have no comtent</span>
+                {/* <i className="iconfont clean" onClick={deleteTodoItem}>&#xe8d1;</i> */}
+              </li>
+            )
+              :
+            <li className="todoitem">
+              {/* <input type="checkbox" className="checkbox" onClick={changeTodoItem}/> */}
+              <span className="discription">have no comtent</span>
+              {/* <i className="iconfont clean" onClick={deleteTodoItem}>&#xe8d1;</i> */}
+            </li>
+          }
         </ul>
       </div>
     </div>
